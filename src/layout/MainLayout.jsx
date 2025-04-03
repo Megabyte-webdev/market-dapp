6790e8a72697d8443d0789dec99e96f04445e4c3
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-04-03 01:34:26 +0100

    Update MainLayout.jsx

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
     return (
-        <div className="w-full max-w-peak px-3 md:px-6" >
+        <div className="w-full max-w-peak" >
             <Navbar />
             <main className="">
                 <Outlet />
