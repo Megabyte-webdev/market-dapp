c4d2ee3fd8d00f01b351b3525fbb51f1190db147
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-04-14 16:55:36 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-20 min-h-8 mx-auto p-3 flex px-sm md:px-md lg:px-lg justify-between items-center flex-wrap md:flex-nowrap transition-all duration-300 bg-white shadow-md">
+    <nav className="fixed top-0 left-0 right-0 z-20 mx-auto px-3 flex px-sm md:px-md lg:px-lg justify-between items-center flex-wrap md:flex-nowrap transition-all duration-300 bg-white shadow-md">
       <NavLink to="/" className="flex justify-center items-center mx-2 mr-auto">
         <img src={logo} alt="Logo" className="w-28 object-contain" />
       </NavLink>
